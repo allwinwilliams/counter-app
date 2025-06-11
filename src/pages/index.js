95b5ba43df3b376ad7d3fa89d51de45f80b2530f
Allwin Williams <allwinwilliams.info@gmail.com>
2025-06-11 11:05:46 +0530

    Link to predictions

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,12 @@ import CounterComponent from '@/components/counter';
 export default function Home() {
   return (
     <>
+      <Link href="/predictions/input">
+        <button><h2>Go to Prediction</h2></button>
+      </Link>
       <h1>Hello counter app!</h1>
       <p>I will be building my counter app here</p>
+      
       <Link href="/cit">
         <button>Go to CIT</button>
       </Link>
